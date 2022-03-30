d28fc3f7688d735fa72fd679c8c39195bf5b3ef3
matteakaiser <matteakaiser@penguinhall.org>
2022-03-30 11:14:27 -0400

    deck of cards done!!!

--- deck-of-cards/deck-of-cards.js
+++ deck-of-cards/deck-of-cards.js
@@ -1,1 +1,16 @@
-let
+listCards(" Hearts")
+listCards(" Spades")
+listCards(" Clubs")
+listCards(" Diamonds")
+
+function listCards(suit) {
+    console.log("Ace of " + suit)
+    for (let i = 2; i < 11; i++) {
+        console.log(i + " of " + suit)
+    }
+    console.log("Jack of " + suit)
+    console.log("Queen of " + suit)
+    console.log("King of " + suit)
+
+}
+
