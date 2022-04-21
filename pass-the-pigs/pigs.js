37d1e93f237fde1fd97bea885cedf9f8972a8216
matteakaiser <matteakaiser@penguinhall.org>
2022-04-21 09:28:32 -0400

    wip

--- pass-the-pigs/pigs.js
+++ pass-the-pigs/pigs.js
@@ -1,5 +1,6 @@
 let currentPlayer = 0
 let nextPlayer = 1
+//what happens when you click the roll and pass buttons
 function handleClick(id) {
     if (id == "player0PassButton") {
         console.log("Player 0 passed");
@@ -14,21 +15,37 @@ function handleClick(id) {
         console.log("Player 3 Passed")
         switchColor("player3", "player0");
     } else if (id == "player0RollButton") {
-    handleRoll()
+        handleRoll()
+    } else if (id == "player1RollButton") {
+        handleRoll()
+    } else if (id == "player2RollButton") {
+        handleRoll()
+    } else if (id == "player3RollButton") {
+        handleRoll()
     }
 
-}
 
-function switchColor(currentPlayer, nextPlayer) {
-    let currentPlayerCard = document.getElementById(currentPlayer);
-    let nextPlayerCard = document.getElementById(nextPlayer);
+
+}
+//switching player 
+function switchColor(currentPlayerId, nextPlayerId) {
+    let currentPlayerCard = document.getElementById(currentPlayerId);
+    let nextPlayerCard = document.getElementById(nextPlayerId);
     currentPlayerCard.setAttribute("class", "w3-card w3-container w3-light-gray w3-round-large")
     nextPlayerCard.setAttribute("class", "w3-card w3-container w3-dark-gray w3-round-large")
+    currentPlayer=currentPlayer + 1
 }
 
-
-function handleRoll(){
-   console.log('player'+currentPlayer+'Pig1')
-    document.getElementById ('player'+currentPlayer+'Pig1').innerHTML='Dot'
-    document.getElementById ('player'+currentPlayer+'Pig2').innerHTML='Dot'
+//basic rolling
+function handleRoll() {
+    console.log('player' + currentPlayer + 'Pig1')
+    document.getElementById('player' + currentPlayer + 'Pig1').innerHTML = 'Dot'
+    document.getElementById('player' + currentPlayer + 'Pig2').innerHTML = 'Dot'
 }
+//start of randomizing 
+function rollPig() {
+    let pig1 = rollPig()
+    let pig2=rollPig()
+    return "No dot"
+}
+
